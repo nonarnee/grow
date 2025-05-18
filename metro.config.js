cbdbb3fd8703456f67c2e746d5db9b5dccee209a
nonarnee <nonarnee@gmail.com>
2025-05-19 06:34:41 +0900

    패키지 매니저 npm으로 변경

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json'];
-config.maxWorkers = 2;
-config.reporter = { verbose: true };
-
-module.exports = config;
